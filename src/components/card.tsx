09c7020ea369d9590a5c99c699299baecae6a6d6
Rian Britto Pereira <140846101+RixelLitz@users.noreply.github.com>
2024-09-27 23:16:25 -0300

    feat: Bug fix and new section added: projects

---
+++ src/components/card.tsx
@@ -0,0 +1,66 @@
+import type React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+
+// Define as props do componente
+interface MediaCardProps {
+  image: string;
+  title: string;
+  description: string;
+  codeLink: string;
+  deployLink: string;
+  technologies: string[];
+}
+
+const MediaCard: React.FC<MediaCardProps> = ({ 
+  image, 
+  title, 
+  description, 
+  codeLink, 
+  deployLink, 
+  technologies 
+}) => {
+  return (
+    <Card className="hover:scale-110 transition-all" sx={{ maxWidth: 400 }}>
+      <CardMedia sx={{ height: 250 }} image={image} title={title} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" sx={{ display: 'flex', alignItems: 'center' }}>
+          {title}
+          {technologies.map((tech, index) => (
+            <Chip 
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+              key={index} 
+              label={tech} 
+              variant="outlined" 
+              sx={{ 
+                ml: 1, 
+                bgcolor: 'blue', 
+                color: 'white', 
+                fontSize: '0.625rem', // Diminuído ainda mais
+                padding: '2px 4px' // Ajustando o padding
+              }} 
+            />
+          ))}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <a href={codeLink} target="_blank" rel="noopener noreferrer">
+          <Button size="small">Veja o código</Button>
+        </a>
+        <a href={deployLink} target="_blank" rel="noopener noreferrer">
+          <Button size="small">Deploy do projeto</Button>
+        </a>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default MediaCard;
--- src/sections/projects/projects.tsx
+++ src/sections/projects/projects.tsx
@@ -1,14 +1,36 @@
-export default function Projects (){
-  return(
+import MediaCard from '../../components/card'
+
+export default function Projects() {
+  return (
     <section className="bg-white p-14 flex flex-col items-center">
       <div className="w-full">
         <div className="flex flex-col text-center items-center">
-          <h1 className="text-black font-medium text-3xl bg-purple-400 rounded-full p-2">Destaques</h1>
-          <h2 className="text-black font-normal text-2xl">Confira os meus projetos em destaque!</h2>
+          <h1 className="text-black font-medium text-3xl bg-purple-400 rounded-full p-2">
+            Destaques
+          </h1>
+          <h2 className="text-black font-normal text-2xl">
+            Confira meus principais projetos freelances
+          </h2>
         </div>
-        <div className="w-full outline">
-          <div>
-            
+        <div className="w-full mt-10 flex items-center justify-center ">
+          <div className="flex gap-64">
+            <MediaCard
+              image="./src/assets/projects/ayla.png"
+              title="AylaBot"
+              description="AylaBot se trata de um site completo para um bot. Onde as principais exigências era uma responsividade bem elaborada..."
+              codeLink="https://github.com/RixelLitz/site-ayla"
+              deployLink="https://ayla.hostsquare.com.br/"
+              technologies={['HTML5', 'CSS3', 'JS']}
+            />
+
+            <MediaCard
+              image="./src/assets/projects/another-project.png"
+              title="AlphaTec"
+              description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
+              codeLink="https://github.com/seu-repositorio/outro-projeto"
+              deployLink="https://outroprojeto.com"
+              technologies={['React', 'TypeScript', 'TailWindCSS']}
+            />
           </div>
         </div>
       </div>
