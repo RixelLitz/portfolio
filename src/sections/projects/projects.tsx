047ee40912f0d3cf67ced07f058d213a9a010859
Rian Britto Pereira <140846101+RixelLitz@users.noreply.github.com>
2024-09-29 17:47:33 -0300

    fix: Bug fix in the responsivity and section project refactored

--- src/sections/projects/projects.tsx
+++ src/sections/projects/projects.tsx
@@ -8,8 +8,10 @@ export default function Projects() {
         src="./src/assets/backgrounds/shape-left.png"
         alt=""
       />
-      <div className="w-full relative">
+      <div className="flex  flex-col gap-36 mt-40 items-center justify-center">
+        {/* Div que gerencia a section destaques */}
         <div className="flex flex-col text-center items-center">
+          {/* Div que gerencia o titulo destaques */}
           <h1 className="text-black font-medium text-3xl bg-purple-400 rounded-full p-2">
             Destaques
           </h1>
@@ -17,59 +19,86 @@ export default function Projects() {
             Confira meus principais projetos freelances
           </h2>
         </div>
-        <div className="w-full mt-10 flex items-center justify-center flex-col ">
-          <div className="flex gap-32 flex-wrap">
-            <MediaCard
-              image="./src/assets/projects/ayla.png"
-              title="AylaBot"
-              description="AylaBot se trata de um site completo para um bot. Onde as principais exigências era uma responsividade bem elaborada..."
-              codeLink="https://github.com/RixelLitz/site-ayla"
-              deployLink="https://ayla.hostsquare.com.br/"
-              technologies={['HTML5', 'CSS3', 'JS']}
-            />
+        <div className="flex justify-center place-items-center gap-32 flex-wrap ">
+          {/* Div que acopla os cards destaques */}
+          <MediaCard
+            image="./src/assets/projects/ayla.png"
+            title="AylaBot"
+            description="AylaBot se trata de um site completo para um bot. Onde as principais exigências era uma responsividade bem elaborada..."
+            codeLink="https://github.com/RixelLitz/site-ayla"
+            deployLink="https://ayla.hostsquare.com.br/"
+            technologies={['HTML5', 'CSS3', 'JS']}
+          />
 
-            <MediaCard
-              image="./src/assets/projects/alphatec.jpg"
-              title="AlphaTec"
-              description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
-              codeLink="https://github.com/seu-repositorio/outro-projeto"
-              deployLink="https://outroprojeto.com"
-              technologies={['React', 'TypeScript', 'TailWindCSS']}
-            />
-          </div>
-          <div className="flex flex-col flex-wrap col-span-2 gap-36 mt-40 items-center justify-center">
-            <div className="flex flex-col text-center w-1/5">
-              <div className="h-1  bg-gradient-to-r from-orange-500 to-blue-500 my-4" />
-              <h1 className='font-fredoka font-medium  text-3xl'>Meus projetos</h1>
-            </div>
-            <div className="flex gap-20 flex-wrap">
-              <MediaCard
-                image="./src/assets/projects/alphatec.jpg"
-                title="AlphaTec"
-                description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
-                codeLink="https://github.com/seu-repositorio/outro-projeto"
-                deployLink="https://outroprojeto.com"
-                technologies={['React', 'TypeScript', 'TailWindCSS']}
-              />
-              <MediaCard
-                image="./src/assets/projects/alphatec.jpg"
-                title="AlphaTec"
-                description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
-                codeLink="https://github.com/seu-repositorio/outro-projeto"
-                deployLink="https://outroprojeto.com"
-                technologies={['React', 'TypeScript', 'TailWindCSS']}
-              />
+          <MediaCard
+            image="./src/assets/projects/alphatec.jpg"
+            title="AlphaTec"
+            description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
+            codeLink="https://github.com/seu-repositorio/outro-projeto"
+            deployLink="https://outroprojeto.com"
+            technologies={['React', 'TypeScript', 'TailWindCSS']}
+          />
+        </div>
+      </div>
 
-              <MediaCard
-                image="./src/assets/projects/alphatec.jpg"
-                title="AlphaTec"
-                description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
-                codeLink="https://github.com/seu-repositorio/outro-projeto"
-                deployLink="https://outroprojeto.com"
-                technologies={['React', 'TypeScript', 'TailWindCSS']}
-              />
-            </div>
-          </div>
+      <div className="flex  flex-col gap-36 mt-40 items-center justify-center">
+        {/* Div que gerencia a parte dos outros projetos */}
+        <div className="flex  flex-col text-center w-1/5">
+          {/* div com titulo da section projects */}
+          <div className="h-1 bg-gradient-to-r from-orange-500 to-blue-500 my-4" />
+          <h1 className="font-fredoka font-medium  text-3xl">Meus projetos</h1>
+        </div>
+        <div className=" p-6 items-center justify-center flex gap-20 flex-wrap">
+          {/* Div que acopla os cards */}
+          <MediaCard
+            image="./src/assets/projects/alphatec.jpg"
+            title="AlphaTec"
+            description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
+            codeLink="https://github.com/seu-repositorio/outro-projeto"
+            deployLink="https://outroprojeto.com"
+            technologies={['React', 'TypeScript', 'TailWindCSS']}
+          />
+          <MediaCard
+            image="./src/assets/projects/alphatec.jpg"
+            title="AlphaTec"
+            description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
+            codeLink="https://github.com/seu-repositorio/outro-projeto"
+            deployLink="https://outroprojeto.com"
+            technologies={['React', 'TypeScript', 'TailWindCSS']}
+          />
+
+          <MediaCard
+            image="./src/assets/projects/alphatec.jpg"
+            title="AlphaTec"
+            description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
+            codeLink="https://github.com/seu-repositorio/outro-projeto"
+            deployLink="https://outroprojeto.com"
+            technologies={['React', 'TypeScript', 'TailWindCSS']}
+          />
+          <MediaCard
+            image="./src/assets/projects/alphatec.jpg"
+            title="AlphaTec"
+            description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
+            codeLink="https://github.com/seu-repositorio/outro-projeto"
+            deployLink="https://outroprojeto.com"
+            technologies={['React', 'TypeScript', 'TailWindCSS']}
+          />
+          <MediaCard
+            image="./src/assets/projects/alphatec.jpg"
+            title="AlphaTec"
+            description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
+            codeLink="https://github.com/seu-repositorio/outro-projeto"
+            deployLink="https://outroprojeto.com"
+            technologies={['React', 'TypeScript', 'TailWindCSS']}
+          />
+          <MediaCard
+            image="./src/assets/projects/alphatec.jpg"
+            title="AlphaTec"
+            description="Descrição do outro projeto. Foi um desafio interessante e aprendi bastante..."
+            codeLink="https://github.com/seu-repositorio/outro-projeto"
+            deployLink="https://outroprojeto.com"
+            technologies={['React', 'TypeScript', 'TailWindCSS']}
+          />
         </div>
       </div>
     </section>
